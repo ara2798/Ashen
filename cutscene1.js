a716362727cdc5b50c9b3e1c64f5b287ee07663f
ara2798 <36863214+ara2798@users.noreply.github.com>
2018-12-01 00:48:45 -0600

    Add files via upload
    Added cutscene (still in progress).

--- cutscene1.js
+++ cutscene1.js
@@ -23,12 +23,12 @@ demo.state997.prototype = {
         music = game.add.audio('cutscene1');
         music.play('', 0, 1, true);
         
-        ash = game.add.sprite(380, 270, 'mc');
+        ash = game.add.sprite(400, 280, 'mc');
         ash.anchor.setTo(0.5,0.5);
         ash.scale.setTo(1.5, 1.5);
         ash.animations.add('walkleft', [6,7,8]);
         ash.animations.play('walkleft',18,true);
-        sister = game.add.sprite(370, 300, 'sister');
+        sister = game.add.sprite(390, 310, 'sister');
         sister.anchor.setTo(0.5,0.5);
         sister.scale.setTo(1.2, 0.8);
         sister.animations.add('walkleft', [0,1,2,3]);
@@ -40,27 +40,28 @@ demo.state997.prototype = {
         
         var darkBg = game.add.sprite(0,0,'square');
         darkBg.scale.setTo(8,6);
-        var text = game.add.text(100, 200, "A long time ago, there was peace in the kingdom...",{fontSize:23,fill:'#ffffff',stroke:'#000000',strokeThickness:4});        
         
-        game.time.events.add(Phaser.Timer.SECOND * 2.5, fadeText1, this);    
+        spellOut(100,200,"A long time ago, there was peace in the kingdom...");
+        
+        game.time.events.add(Phaser.Timer.SECOND * 3, fadeText1, this);    
 
         function fadeText1(){
             textfade = game.add.tween(text).to( { alpha: 0 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
             textfade.onComplete.add(function(){text.kill();},this);
             game.add.tween(darkBg).to( { alpha: 0 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
             console.log("change");
-            changeState(null,'graveyard');
-        }
-        
-        game.time.events.add(Phaser.Timer.SECOND * 6, createText1, this);
-
-        function createText1(){
-            darken = game.add.tween(darkBg).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
-            darken.onComplete.add(function(){text = game.add.text(100, 200, "But then, a mysterious knight appeared, and with his\narmy from the underworld, he brought destruction and\nwrath upon the kingdom.",{fontSize:23,fill:'#ffffff',stroke:'#000000',strokeThickness:4});throneroom.kill();}, this);                  
             //changeState(null,'graveyard');
         }
         
-        game.time.events.add(Phaser.Timer.SECOND * 12, fadeText2, this);    
+        game.time.events.add(Phaser.Timer.SECOND * 6.5, createText1, this);
+
+        function createText1(){
+            darken = game.add.tween(darkBg).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+            darken.onComplete.add(function(){spellOut(100, 200, "But then, a mysterious knight appeared, and with his\narmy from the underworld, he brought destruction and\nwrath upon the kingdom...");throneroom.kill();}, this);                  
+            //changeState(null,'graveyard');
+        }
+        
+        game.time.events.add(Phaser.Timer.SECOND * 14, fadeText2, this);    
 
         function fadeText2(){
             textfade = game.add.tween(text).to( { alpha: 0 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
@@ -68,32 +69,40 @@ demo.state997.prototype = {
             game.add.tween(darkBg).to( { alpha: 0 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
         }
         
-        game.time.events.add(Phaser.Timer.SECOND * 12, fadeText, this);    
+        game.time.events.add(Phaser.Timer.SECOND * 17, createText2, this);    
 
-        function fadeText(){
+        function createText2(){
+            darken = game.add.tween(darkBg).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+            darken.onComplete.add(function(){spellOut(100, 200, "He took the throne and the prince tried to escape\nwith the princess...");castleinterior.kill();}, this);
+        }
+        
+        game.time.events.add(Phaser.Timer.SECOND * 21.5, fadeText3, this);    
+
+        function fadeText3(){
             textfade = game.add.tween(text).to( { alpha: 0 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
             textfade.onComplete.add(function(){text.kill();},this);
             game.add.tween(darkBg).to( { alpha: 0 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
         }
-        /*
-        game.time.events.add(Phaser.Timer.SECOND * 9, removeTween, this);
-
-        function removeTween(){
-            logo.kill();
-            music = game.add.audio('title');
-            music.play('', 0, 1, true);
-            var emitter = game.add.emitter(200, -200, 80);
-            emitter.width = 600;
-            emitter.angle = 10;
-            emitter.makeParticles('rain');
-            emitter.minParticleScale = 0.1;
-            emitter.maxParticleScale = 0.5;
-            emitter.setYSpeed(300, 500);
-            emitter.setXSpeed(-5, 5);
-            emitter.minRotation = 0;
-            emitter.maxRotation = 0;
-            emitter.start(false, 1000, 5, 0);
-        }*/
+        
+        game.time.events.add(Phaser.Timer.SECOND * 24, createText3, this); 
+        
+        function createText3(){
+            darken = game.add.tween(darkBg).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+            darken.onComplete.add(function(){spellOut(100, 200, "But in the mayhem, the princess was brutally killed...");ash.kill();sister.kill();}, this);
+        }
+        
+        game.time.events.add(Phaser.Timer.SECOND * 27.5, createText4, this); 
+        
+        function createText4(){
+            textfade = game.add.tween(text).to( { alpha: 0 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+            textfade.onComplete.add(function(){text.kill();spellOut(100, 200, "The prince, Ash, decided to stay behind and seek\nrevenge for his sister’s death")},this);
+        }
+        
+        game.time.events.add(Phaser.Timer.SECOND * 33.5, change, this);
+        
+        function change(){
+            changeState(null,"graveyard");
+        }
         
     },
     update: function(){
@@ -101,3 +110,17 @@ demo.state997.prototype = {
         
     }
 };
+
+function spellOut(x,y,lines){
+    text = game.add.text(x,y,'',{fontSize:23,fill:'#ffffff',stroke:'#000000',strokeThickness:4});
+    var loop = game.time.events.loop(40,addChar);
+    var index = 0;
+    
+    function addChar(){
+        text.text += lines[index];
+        if (index >= lines.length - 1){
+            game.time.events.remove(loop);
+        }
+        index++;
+    }
+}
